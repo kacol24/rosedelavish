cb7b4518f4d77da60f80f71e624d9b64a2c5f65c
Kevin Chandra <kacol.bot@gmail.com>
2023-06-27 01:47:25 +0700

    skeleton catalog

--- resources/views/components/product-item.blade.php
+++ resources/views/components/product-item.blade.php
@@ -1,48 +1,67 @@
 @props(['product'])
 
-<div @class(['product__item', 'sale'])>
-    <div class="product__item__pic set-bg" style="background-image: url({{ $product->thumbnail->getUrl('product-thumbnail') }})">
-        {{--        <span class="label">Sale</span>--}}
-        <ul class="product__hover">
-            <li>
-                <img src="{{ asset('img/icon/heart.png') }}" alt=""> <span>Wishlist</span>
-            </li>
-            {{--            <li>--}}
-            {{--                <a href="#">--}}
-            {{--                    <img src="img/icon/compare.png" alt=""> <span>Compare</span>--}}
-            {{--                </a>--}}
-            {{--            </li>--}}
-            {{--            <li>--}}
-            {{--                <a href="#">--}}
-            {{--                    <img src="img/icon/search.png" alt="">--}}
-            {{--                </a>--}}
-            {{--            </li>--}}
-        </ul>
+@isset($product)
+    <div @class(['product__item', 'sale'])>
+        <div class="product__item__pic set-bg"
+             style="background-image: url({{ $product->thumbnail->getUrl('product-thumbnail') }})">
+            {{--        <span class="label">Sale</span>--}}
+            <ul class="product__hover">
+                <li>
+                    <img src="{{ asset('img/icon/heart.png') }}" alt=""> <span>Wishlist</span>
+                </li>
+                {{--            <li>--}}
+                {{--                <a href="#">--}}
+                {{--                    <img src="img/icon/compare.png" alt=""> <span>Compare</span>--}}
+                {{--                </a>--}}
+                {{--            </li>--}}
+                {{--            <li>--}}
+                {{--                <a href="#">--}}
+                {{--                    <img src="img/icon/search.png" alt="">--}}
+                {{--                </a>--}}
+                {{--            </li>--}}
+            </ul>
+        </div>
+        <div class="product__item__text">
+            <h6>
+                {{ $product->translateAttribute('name') }}
+            </h6>
+            {{--        <div class="rating">--}}
+            {{--            <i class="fa fa-star"></i>--}}
+            {{--            <i class="fa fa-star"></i>--}}
+            {{--            <i class="fa fa-star"></i>--}}
+            {{--            <i class="fa fa-star"></i>--}}
+            {{--            <i class="fa fa-star-o"></i>--}}
+            {{--        </div>--}}
+            <h5>
+                <x-product-price :product="$product"/>
+            </h5>
+            {{--        <div class="product__color__select">--}}
+            {{--            <label for="pc-7">--}}
+            {{--                <input type="radio" id="pc-7">--}}
+            {{--            </label>--}}
+            {{--            <label class="active black" for="pc-8">--}}
+            {{--                <input type="radio" id="pc-8">--}}
+            {{--            </label>--}}
+            {{--            <label class="grey" for="pc-9">--}}
+            {{--                <input type="radio" id="pc-9">--}}
+            {{--            </label>--}}
+            {{--        </div>--}}
+        </div>
     </div>
-    <div class="product__item__text">
-        <h6>
-            {{ $product->translateAttribute('name') }}
-        </h6>
-        {{--        <div class="rating">--}}
-        {{--            <i class="fa fa-star"></i>--}}
-        {{--            <i class="fa fa-star"></i>--}}
-        {{--            <i class="fa fa-star"></i>--}}
-        {{--            <i class="fa fa-star"></i>--}}
-        {{--            <i class="fa fa-star-o"></i>--}}
-        {{--        </div>--}}
-        <h5>
-            <x-product-price :product="$product"/>
-        </h5>
-        {{--        <div class="product__color__select">--}}
-        {{--            <label for="pc-7">--}}
-        {{--                <input type="radio" id="pc-7">--}}
-        {{--            </label>--}}
-        {{--            <label class="active black" for="pc-8">--}}
-        {{--                <input type="radio" id="pc-8">--}}
-        {{--            </label>--}}
-        {{--            <label class="grey" for="pc-9">--}}
-        {{--                <input type="radio" id="pc-9">--}}
-        {{--            </label>--}}
-        {{--        </div>--}}
+@else
+    <div @class(['product__item', 'placeholder-glow'])>
+        <div class="product__item__pic">
+            <div class="ratio ratio-1x1">
+                <div class="placeholder"></div>
+            </div>
+        </div>
+        <div class="product__item__text">
+            <h6>
+                <span class="placeholder col-8"></span>
+            </h6>
+            <h5>
+                <span class="placeholder col-3"></span>
+            </h5>
+        </div>
     </div>
-</div>
+@endisset
--- resources/views/livewire/components/catalog.blade.php
+++ resources/views/livewire/components/catalog.blade.php
@@ -1,47 +1,45 @@
-<div>
-    <div wire:init="loadProducts">
+<div wire:init="loadProducts">
+    <div wire:loading.block>
         <div class="row">
-            @foreach($products as $product)
+            @foreach(range(1, 3) as $product)
                 <div class="col-lg-4 col-md-6 col-sm-6">
-                    <a href="{{ $product->defaultUrl->slug }}">
-                        <x-product-item :product="$product"/>
-                    </a>
+                    <x-product-item/>
                 </div>
             @endforeach
         </div>
-        <div class="shop__product__option">
-            <div class="row">
-                <div class="col-lg-6 col-md-6 col-sm-6">
-                    @if($products)
-                        <div class="shop__product__option__left">
-                            <p>
-                                Showing {{ $products->firstItem() }}–{{ $products->lastItem() }}
-                                of {{ $products->total() }} results
-                            </p>
-                        </div>
-                    @endif
-                </div>
-                <div class="col-lg-6 col-md-6 col-sm-6">
-                    @if($products && $products->hasPages())
-                        <div class="product__pagination">
-                            <a class="active" href="#">1</a>
-                            <a href="#">2</a>
-                            <a href="#">3</a>
-                            <span>...</span>
-                            <a href="#">21</a>
-                        </div>
-                    @endif
-                </div>
-            </div>
-        </div>
     </div>
-    <div wire:loading.class="d-flex"
-         wire:loading.class.remove="d-none"
-         class="d-none"
-         style="position: absolute; top: 0;left: 0;width: 100%;height: 100%;align-items: center;justify-content: center;background-color:rgba(255, 255, 255, .7);">
-        <div>
-            <i class="fa fa-refresh fa-lg fa-spin" aria-hidden="true"></i>
-            Loading...
+    <div class="row" wire:loading.remove>
+        @foreach($products as $product)
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <a href="{{ $product->defaultUrl->slug }}">
+                    <x-product-item :product="$product"/>
+                </a>
+            </div>
+        @endforeach
+    </div>
+    <div class="shop__product__option">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                @if($products)
+                    <div class="shop__product__option__left">
+                        <p>
+                            Showing {{ $products->firstItem() }}–{{ $products->lastItem() }}
+                            of {{ $products->total() }} results
+                        </p>
+                    </div>
+                @endif
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                @if($products && $products->hasPages())
+                    <div class="product__pagination">
+                        <a class="active" href="#">1</a>
+                        <a href="#">2</a>
+                        <a href="#">3</a>
+                        <span>...</span>
+                        <a href="#">21</a>
+                    </div>
+                @endif
+            </div>
         </div>
     </div>
 </div>
