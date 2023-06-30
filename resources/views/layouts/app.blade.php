caba85ad872041a58d2d01061b8e4d2abf0f58f3
Kevin Chandra <kacol.bot@gmail.com>
2023-06-30 18:26:44 +0700

    added slick carousel import

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap"
           rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-{{--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">--}}
+    {{--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">--}}
     <link rel="stylesheet"
-          href="https://cdn.jsdelivr.net/combine/npm/magnific-popup@1.1.0/dist/magnific-popup.min.css,npm/jquery-nice-select@1.1.0/css/nice-select.min.css,npm/owl.carousel@2.3.4/dist/assets/owl.carousel.min.css">
+          href="https://cdn.jsdelivr.net/combine/npm/magnific-popup@1.1.0/dist/magnific-popup.min.css,npm/jquery-nice-select@1.1.0/css/nice-select.min.css,npm/owl.carousel@2.3.4/dist/assets/owl.carousel.min.css,npm/slick-carousel@1.8.1/slick/slick.min.css,npm/slick-carousel@1.8.1/slick/slick-theme.min.css">
     <!-- Css Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
@@ -59,8 +59,11 @@
 <!-- Js Plugins -->
 <script src="https://code.jquery.com/jquery-3.7.0.min.js"
         integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+        crossorigin="anonymous"></script>
 <script
-    src="https://cdn.jsdelivr.net/combine/npm/bootstrap@4.6.2,npm/magnific-popup@1.1.0,npm/jquery-nice-select@1.1.0,npm/jquery.nicescroll@3.7.6,npm/jquery-countdown@2.2.0,npm/mixitup@3.3.1,npm/owl.carousel@2.3.4,npm/slicknav@1.0.8/dist/jquery.slicknav.min.js"></script>
+    src="https://cdn.jsdelivr.net/combine/npm/magnific-popup@1.1.0,npm/jquery-nice-select@1.1.0,npm/jquery.nicescroll@3.7.6,npm/jquery-countdown@2.2.0,npm/mixitup@3.3.1,npm/owl.carousel@2.3.4,npm/slicknav@1.0.8/dist/jquery.slicknav.min.js,npm/slick-carousel@1.8.1"></script>
 <script src="{{ mix('js/app.js') }}"></script>
 @livewireScripts
 </body>
